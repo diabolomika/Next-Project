532a3a8608408cc916eecc7b06694eaf82532f10
Diabolomika <diabolomika@gmail.com>
2024-07-19 09:30:26 +0200

    Update headerNavLinks.ts
    update

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,9 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
+  { href: '/blog', title: 'Blog/Blogg' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/about', title: 'About/Om' },
 ]
 
 export default headerNavLinks
