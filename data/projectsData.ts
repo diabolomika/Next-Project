d21109e2ebd213f316214f45b78faddf5bbf0734
Diabolomika <diabolomika@gmail.com>
2024-07-24 19:14:08 +0200

    Update projectsData.ts
    update projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,27 +1,38 @@
-interface Project {
-  title: string,
-  description: string,
-  href?: string,
-  imgSrc?: string,
+const projectsData = {
+  en: [
+    {
+      title: 'Skandinaviske Circus Prosjektet',
+      description: `We have teamed up with Lars Spoere from Kragero Aktion Park to offer our circus workshop program at their location. For more information go to scandinaviskecircusprosjektet.org
+We rent their grounds for our circus project in a circustent. If you also want to operate a go-cart, you get a small discount if you run a circus workshop with DSS.`,
+      imgSrc: '/static/images/google.png',
+      href: 'https://www.google.com',
+    },
+    {
+      title: 'Translation services and webdsign',
+      description: `Are you looking for a dedicated, qualified and skilled translators who have a good eye for detail!
+      shrinks from tight deadlines? Do you have a webdesign project that needs a webdesigner? Then you have found the right place! Trnslation for english, german, norwegian, 
+      russian and ukrainian at reasonable rates.`,
+      imgSrc: '/static/images/time-machine.jpg',
+      href: '/blog/the-time-machine',
+    },
+  ],
+  no: [
+    {
+      title: 'Skandinaviske Circus Prosjektet',
+      description: `Vi har slått oss sammen med Lars Spoere fra Kragero Aktion Park for å tilby vårt sirkusverkstedsprogram på deres lokasjon. For mer informasjon gå til skandinaviskecirkusprosjektet.org
+Vi kan bruke deres grunn til vårt sirkusprosjekt i circustent, som kommer veldig godt med. Ønsker du også å drive med gocart får du en liten rabatt hvis du holder på med en sirkusverksted med DSS.,
+      imgSrc: '/static/images/google.png',
+      href: 'https://www.google.com',
+    },
+    {
+      title: 'Translation services and webdsign',
+      description: ``Ser du etter en dedikert, kvalifisert og dyktig oversettere som har et godt øye for detaljer!
+      krymper fra stramme tidsfrister? Har du et webdesignprosjekt som trenger en webdesigner? Da har du funnet rett 
+      sted! Oversettelse for engelsk, tysk, norsk, russisk og ukrainsk til rimelige priser.`,
+      imgSrc: '/static/images/time-machine.jpg',
+      href: '/blog/the-time-machine',
+    },
+  ],
 }
-
-const projectsData: Project[] = [
-  {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
-]
 
 export default projectsData
