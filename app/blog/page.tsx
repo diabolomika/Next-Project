2e20797d860c4c618b1d3dfdf3d427979cd624e6
Diabolomika <diabolomika@gmail.com>
2024-08-06 21:27:09 +0200

    Update page.tsx
    locales added

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,14 +2,45 @@ import ListLayout from '@/layouts/ListLayoutWithTags'
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import { genPageMetadata } from 'app/seo'
+import useTranslation from 'next-translate/useTranslation'
 
-const POSTS_PER_PAGE = 5
+const POSTS_PER_PAGE = 3
 
 export const metadata = genPageMetadata({ title: 'Blog' })
 
-export default function BlogPage() {
-  const posts = allCoreContent(sortPosts(allBlogs))
-  const pageNumber = 1
+export async function getStaticPaths({ locales, defaultLocale }) {
+  const paths = (
+    await Promise.all(
+      locales.map(async (locale) => {
+        const otherLocale = locale !== defaultLocale ? locale : ''
+        const totalPosts = await getAllFilesFrontMatter('blog', otherLocale) // don't forget to useotherLocale
+        const totalPages = Math.ceil(totalPosts.length / POSTS_PER_PAGE)
+        return Array.from({ length: totalPages }, (_, i) => [(i + 1).toString(), locale])
+      })
+    )
+  ).flat()
+
+  return {
+    paths: paths.map(([page, locale]) => ({
+      params: {
+        page,
+      },
+      locale,
+    })),
+    fallback: false,
+  }
+}
+
+export async function getStaticProps(context) {
+  const {
+    params: { page },
+    defaultLocale,
+    locales,
+    locale,
+  } = context
+  const otherLocale = locale !== defaultLocale ? locale : ''
+  const posts = await getAllFilesFrontMatter('blog', otherLocale)
+  const pageNumber = parseInt(page)
   const initialDisplayPosts = posts.slice(
     POSTS_PER_PAGE * (pageNumber - 1),
     POSTS_PER_PAGE * pageNumber
@@ -19,12 +50,52 @@ export default function BlogPage() {
     totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
   }
 
+  // Checking if available in other locale for SEO
+  const availableLocales = []
+  await locales.forEach(async (ilocal) => {
+    const otherLocale = ilocal !== defaultLocale ? ilocal : ''
+    const iAllPosts = await getAllFilesFrontMatter('blog', otherLocale)
+    iAllPosts.forEach(() => {
+      if (
+        pageNumber <= Math.ceil(iAllPosts.length / POSTS_PER_PAGE) &&
+        !availableLocales.includes(ilocal)
+      )
+        availableLocales.push(ilocal)
+    })
+  })
+
+  return {
+    props: {
+      posts,
+      initialDisplayPosts,
+      pagination,
+      locale,
+      availableLocales,
+    },
+  }
+}
+
+export default function PostPage({
+  posts,
+  initialDisplayPosts,
+  pagination,
+  locale,
+  availableLocales,
+}) {
+  const { t } = useTranslation()
   return (
-    <ListLayout
-      posts={posts}
-      initialDisplayPosts={initialDisplayPosts}
-      pagination={pagination}
-      title="All Posts"
-    />
+    <>
+      <PageSEO
+        title={siteMetadata.title[locale]}
+        description={siteMetadata.description[locale]}
+        availableLocales={availableLocales}
+      />
+      <ListLayout
+        posts={posts}
+        initialDisplayPosts={initialDisplayPosts}
+        pagination={pagination}
+        title={t('common:all')}
+      />
+    </>
   )
 }
